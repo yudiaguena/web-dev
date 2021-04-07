77c522bddae213425839628cdee5aaa3903e9541
Yudi Aguena <yudi.aguena@gmail.com>
2021-04-06 23:58:52 -0300

    commit

--- snake game/script.js
+++ snake game/script.js
@@ -38,15 +38,15 @@ function drawFood () {
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if(event.keyCode == 65 && direction != 'right') direction = 'left';
-    if(event.keyCode == 87 && direction != 'down') direction = 'up';
-    if(event.keyCode == 68 && direction != 'left') direction = 'right';
-    if(event.keyCode == 83 && direction != 'up') direction = 'down';
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function fimJogo() {
     clearInterval(jogo);
-    if(!alert('Game over :/\n pressione "space" para começar de novo')){window.location.reload();}
+    if(!alert('Game over :/\n pressione "space" ou "enter" para começar de novo : )')){window.location.reload();}
 }
 
 function iniciarJogo() {    
